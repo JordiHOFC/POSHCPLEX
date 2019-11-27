2f03fac36e4196229369b64aece1ac11947275da
Jordi Henrique Silva <jordi.silva@ufv.br>
2019-11-27 03:05:43 -0300

    se nao ta pronto n sei quando tá

--- POSH.cpp
+++ POSH.cpp
@@ -19,15 +19,15 @@ POSH::POSH(string &nomeArq)
     //ler o numero de vertices
     f >> n;
     n++; //
-    //ler o numero de hoteis extras
-    f >> h;
+    //ler o numero de Hoteis extras
+    f >> H;
     //ler o numero total de viagens
     f >> D;
     //ler o tempo total da viagem
     f >> Tmax;
     //ler o tempo de viagem para cada viagem
-    Td = vector<double>(h + 1);
-    totalH = h+2;
+    Td = vector<double>(H + 1);
+    totalH = H+2;
     Si=vector<int>(n);
     double a;
     
@@ -42,7 +42,7 @@ POSH::POSH(string &nomeArq)
     Cliente ponto,pontoI,pontoJ;
     
     
-    //prenchemos nossos pontos de cliente e hoteis.
+    //prencHemos nossos pontos de cliente e Hoteis.
     for(int i = 0; i < n; i++)
     {
         f >> ponto.x;
@@ -95,11 +95,25 @@ POSH::POSH(string &nomeArq)
             x[i][j]=IloNumVarArray(env,D);
         }
     }
+    //DECLARANDO VARIAVEL DE DECISAO UI
+    IloNumVarArray U(env,n,1,n,ILOINT);
+    for(int i=0;i<n;i++){
+        cout<<U[i]<<"TAMANHO DE N:    "<<n<<endl;
+       modelo.add(U[i]); 
+    }
     
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             for(int d=0;d<D;d++)
-                x[i][j][d]=IloNumVar(env,0,1,ILOBOOL);
+                if(i!=j)
+                {
+                    x[i][j][d]=IloNumVar(env,0,1,ILOBOOL);
+                }                   
+                else
+                {
+                    x[i][j][d]=IloNumVar(env,0,0,ILOBOOL);
+                }
+                
         }
     }
     
@@ -122,109 +136,110 @@ POSH::POSH(string &nomeArq)
     modelo.add(IloMaximize(env, fo));
     //fo.end();
 
-   //restrição 1
+   //restrição 1 (2.2) ////////////////////////////////////////////////////////////////////////////////////////////////
     IloExpr soma(env);
-    for(int i=1;i<n;i++){
-       //if(i>=h+2){
-        soma+=x[0][i][0];
-       //}
+    for(int l=1;l<n;l++){
+       if(l>H+2){
+        soma+=x[0][l][0];
+       }
     }
     IloRange viagemH0(env,1,soma,1);
     modelo.add(viagemH0);
     
     
-    //restrição 2
+    //restrição 2 (2.3) ////////////////////////////////////////////////////////////////////////////////////////////////
     IloExpr soma1(env);
-    for(int i=0;i<n;i++){
-        soma1+=x[i][1][D-1];      
-      
+    for(int k=0;k<n;k++){
+        if(k>H+2){
+            soma1+=x[k][1][D-1];      
+       }
     }
     IloRange viagemH1(env,1,soma1,1);
     modelo.add(viagemH1);
     
-    
-    //restricao 3    
-    for(int d=0;d<D;d++){
+   
+    //restricao 3    (2.4) ////////////////////////////////////////////////////////////////////////////////////////////////
+   for(int d=0;d<D;d++){
         IloExpr somaH(env);
-        for(int i=0;i<h+2;i++){
-            for(int j=0;j<n;j++){
-                //if(j>=h+2)
-                    somaH+=x[i][j][d];
+        for(int h=0;h<H+2;h++){
+            for(int l=0;l<n;l++){
+                if(l>H+2)
+                    somaH+=x[h][l][d];
  
             }
         }
-        IloRange hoteisDisponiveis(env,1,somaH,1);    
-        modelo.add(hoteisDisponiveis);
+        IloRange HoteisDisponiveis(env,1,somaH,1);    
+        modelo.add(HoteisDisponiveis);
     }
 
-
-    // restrição 4
+    // restrição 4 (2.5) ////////////////////////////////////////////////////////////////////////////////////////////////
     for(int d=0;d<D;d++){
         IloExpr somaH2(env);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<h+2;j++){
-                //if(i>=h+2)
-                    somaH2+=x[i][j][d];
+        for(int h=0;h<H+2;h++){
+            for(int k=0;k<n;k++){
+            
+                if(k>H+2)
+                    somaH2+=x[k][h][d];
  
             }
         }
-        IloRange hoteisDisponiveis2(env,1,somaH2,1);    
-        modelo.add(hoteisDisponiveis2);
+        IloRange HoteisDisponiveis2(env,1,somaH2,1);    
+        modelo.add(HoteisDisponiveis2);
     }
-    printf("\n\nAqui kkkk");
     
 
- //restricao 5 
+ //restricao 5 (2.6) ////////////////////////////////////////////////////////////////////////////////////////////////
    
-    for(int i=0;i<h+2;i++){
-        for(int d=0;d<D-1;d++){
+    for(int h=0;h<H+2;h++){
+        for(int d=0;d<(D-1);d++){
              IloExpr somaMesmoHotel1(env);
              IloExpr somaMesmoHotel2(env);
-             for(int j=0;j<n;j++){
-               // if(j>=h+2){
-                    somaMesmoHotel1+=x[j][i][d];
-                //}
+             for(int k=0;k<n;k++){
+                if(k>H+2){
+                    somaMesmoHotel1+=x[k][h][d];
+                }
              }
-                for(int j=0;j<n;j++){
-                //if(j>=h+2){
-                    somaMesmoHotel2+=x[i][j][d];
-                //}
+                for(int l=0;l<n;l++){
+                if(l>H+2){
+                    somaMesmoHotel2+=x[h][l][d+1];
+                }
              }
              IloRange mesmoHotel(env,0,somaMesmoHotel1-somaMesmoHotel2,0);
              modelo.add(mesmoHotel);
         }
     }
-    //restrição 6 - 2.7
+    
+    //restrição 6 - (2.7) ////////////////////////////////////////////////////////////////////////////////////////////////
    
     for(int d=0;d<D;d++){
-        for(int i=h+2;i<n;i++){
+        for(int k=H+3;k<n;k++){
              IloExpr somaConectividade1(env);
              IloExpr somaConectividade2(env);
-             for(int j=0;j<n;j++){
-            //if(i>=h+2)
-                somaConectividade1+=x[j][i][d];
-                
-            }
-            for(int j=0;j<n;j++){ 
-                //if(i>=h+2)
-                
-                    somaConectividade2+=x[i][j][d];
-            //   }
-            }
+             for(int i=0;i<n;i++){
+                if(k>H+2 && i>H+2){
+                    somaConectividade1+=x[i][k][d];                    
+                }
+             }
+             for(int j=0;j<n;j++){ 
+                if(k>H+2 && j>H+2){                
+                    somaConectividade2+=x[k][j][d];
+                }
+             }
              IloRange conectividadeUnica(env,0,somaConectividade1-somaConectividade2,0);
              modelo.add(conectividadeUnica);
         }
-    }
-    //restrição 7
+    } 
+    //restrição 7 (2.8) ////////////////////////////////////////////////////////////////////////////////////////////////
     
     for(int i=0;i<n;i++){
         IloExpr visitaUnica(env);
-        for(int j=h+2;j<n;j++){
-            for(int d=0;d<D;d++){
-                 //if(i>1){
+        for(int d=0;d<D;d++){
+            for(int j=0;j<n;j++){
+            
+                 if(i>H+2 && j>H+2){
                     visitaUnica+=x[i][j][d];
                     
-                 //}
+                 }
             }
         }
         IloRange visitaUnicaa(env,0,visitaUnica,1);
@@ -232,23 +247,44 @@ POSH::POSH(string &nomeArq)
         
     }
 
-    //restrição 8
-    
+    //restrição 8 (2.9) ////////////////////////////////////////////////////////////////////////////////////////////////
     for(int d=0;d<D;d++){
         IloExpr rest9(env);
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             for(int j=0;j<n;j++){
+                //if(i>H+2 && j> H+2){
+                    rest9+=matrizDist[i][j]*x[i][j][d];
+              // }
                 
-                    //if(i>=1){
-                        rest9+=matrizDist[i][j]*x[i][j][d];
-                    //    modelo.add(rest9<=Td[d]);
-                         
-                   // }
             }
         }
         IloRange a(env,0,rest9,Td[d]);
         modelo.add(a);
+       
     }
+      //restrição 8 (2.9) ////////////////////////////////////////////////////////////////////////////////////////////////
+    
+   
+    for(int i=H+3;i<n;i++){
+        for(int j=H+3;j<n;j++){
+            IloExpr somaUi(env);
+            IloExpr somaUj(env);
+            IloExpr somaXijd(env);
+            /*for(int u=H+3;u<n;u++){
+                somaUi+=U[u];
+            }
+            for(int u=H+3;u<n;u++){
+                somaUj+=U[u];
+            }*/
+            for(int d=0;d<D;d++){
+                somaXijd+=x[i][j][d];
+            }
+             
+            //IloRange posicaoNaSolucao(env,esquerda,direita);
+            modelo.add((U[i]-U[j]+1)<=((n-1)*(1-somaXijd)));
+        }
+    }
+
    printf("\nCPLEX");
     //modelo.add(rest9<=Tmax);
     // Carregando o módulo do Cplex
@@ -270,8 +306,12 @@ POSH::POSH(string &nomeArq)
     }*/
     
     
-    printf("\n\n fo: %lf",fit);
-
+    printf("\n\n fo: %lf\n",fit);
+    for(int j=0;j<n;j++){
+       int num=cplex.getValue(U[j]);         
+    printf("%d  ",num);
+    }
+    printf("\n");
     f.close();
 }
 
@@ -279,7 +319,3 @@ POSH::~POSH()
 {
 }
 
-/*void POSH::modeloMatematico(){
-      // DECLARANDO O AMBIENTE E O MODELO MATEMATICO
-   
-}*/
